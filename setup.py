1d808b533a8237e91fb52dce3250720da41ab628
Danjje <danutaadudek@outlook.com>
2025-05-06 17:47:00 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Always prefer setuptools over distutils
+import re
+
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+here = path.abspath(path.dirname(__file__))
